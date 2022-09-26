33665d65f666751c1c1a8d81137edcae626a1b86
aziz-wahyudin <midnightmist37@gmail.com>
2022-09-26 16:18:42 +0700

    add feature connect database

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func connectGorm() (*gorm.DB, error) {
+	dsn := "root:@tcp(127.0.0.1:3306)/Book_Rent?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func callClear() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func migrate(db *gorm.DB) {
+	db.AutoMigrate()
+}
+
+func main() {
+	gconn, err := connectGorm()
+	migrate(gconn)
+	if err != nil {
+		fmt.Println("cannot connect to DB", err.Error)
+	}
+}
