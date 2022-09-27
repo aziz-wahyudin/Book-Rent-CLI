1865a4c108e1e2cc931d588e1eee1e37becc01a2
Frangky-Simanjuntak <frangkymuler7@gmail.com>
2022-09-27 14:28:27 +0700

    membuat book controler dan book model

--- controller/book.go
+++ controller/book.go
@@ -1,22 +1,19 @@
-package control
+package controller
 
-import (
-
-)
+import "Alterra/Project1-BE12-Book-Rent/model"
 
 type BookControll struct {
-	Model model.Book
+	Model model.BookModel
 }
 
 func (mc BookControll) GetAll() ([]model.Book, error) {
-	res, err := mc.Model.GetAll ()
-	if err!= nil {
+	res, err := mc.Model.GetAll()
+	if err != nil {
 		return nil, err
 	}
 	return res, nil
 }
 
-//mengambil data 
 func (gc BookControll) Add(data model.Book) (model.Book, error) {
 	res, err := gc.Model.Insert(data)
 	if err != nil {
